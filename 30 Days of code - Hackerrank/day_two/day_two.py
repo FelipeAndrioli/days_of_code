636571f6f177b5cfea1eb3d8395f3aadcfa51f63
Felipe Andrioli <felipeandrioli56@hotmail.com>
2019-12-27 10:15:07 -0300

    Added day two

---
+++ 30 Days of code - Hackerrank/day_two/day_two.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+si = input()
+sd = input()
+ss = input()
+
+print(int(si) + i)
+print(float(sd) + d)
+print(s + ss)
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
