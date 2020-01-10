0312e538b4fb6add965d897ebd29e6d56523d945
Felipe Andrioli <felipeandrioli56@hotmail.com>
2020-01-10 16:06:14 -0300

    Added day twelve

--- 30 Days of code - Hackerrank/day_twelve/day_twelve.js
+++ 30 Days of code - Hackerrank/day_twelve/day_twelve.js
@@ -22,23 +22,47 @@ class Student extends Person {
     }
 
     calculate(){
-        console.log("Num Scores: " + this.numScores)
-        console.log("Test Scores: " + this.testScores)
+
+        let sum = 0
+        let avg = 0
+        let conversion
+
+        for(let i = 0; i < this.numScores; i++){
+            sum += this.testScores[i]
+        }
+
+        avg = sum / this.numScores
+
+        if(90 <= avg  && avg <= 100){
+            //conversion = 'O'
+            return 'O'
+        }else if(80 <= avg  && avg < 90){
+            //conversion = 'E'
+            return 'E'
+        }else if(70 <= avg  && avg < 80){
+            //conversion = 'A'
+            return 'A'
+        }else if(55 <= avg && avg< 70){
+            //conversion = 'P'
+            return 'P'
+        }else if(40 <= avg && avg < 55){
+            //conversion = 'D'
+            return 'D'
+        }else if(avg < 40){
+            //conversion = 'T'
+            return 'T'
+        }
     }
 }
 
 function main(){
-    let firstName = 'Nirso'
-    let lastName = 'Gordo'
-    let id = 666
-    let numScores = 4
-    let testScores = new Array(numScores)
+    let firstName = 'Heraldo'
+    let lastName = 'Mememlli'
+    let id = 8135627
+    let numScores = 2
+    let testScores = [100, 80]
 
-    for(let i = 0; i < numScores; i++){
-        testScores[i] = 8
-    }
-
-    let s = new Student(firstName, lastName, id, testScores)
+    let s = new Student(firstName, lastName, id, numScores, testScores)
     s.printPerson()
     s.calculate()
     console.log('Grade: ' + s.calculate())
